bc1443a6e70260bce6d046576fdb47e39a21a975
Aluizio Monteiro dos Santos Junior <devaluizio@gmail.com>
2024-06-29 16:54:43 -0300

    Concatenações

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,13 +1,17 @@
+
+
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         
-        short num1 = 1;
-        int num2 = num1;
-        short num3 = (short) num2;
 
-        final double PI =  3.1415;
-
-        float numero = 2.500F;
-        System.out.println("Hello, World!"+numero);
+        String concatenation = "?";
+        System.out.println(concatenation);
+        concatenation = 1+1+1+"1";
+        System.out.println(concatenation);
+        concatenation = 1+"1"+1+1;
+        System.out.println(concatenation);
+        concatenation = "1"+(1+1+1);
+        System.out.println(concatenation);
     }
 }
