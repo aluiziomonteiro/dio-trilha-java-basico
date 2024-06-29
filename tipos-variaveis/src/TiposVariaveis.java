e8893cd33f071b58f4908b2e6bbeabe96984496d
Aluizio Monteiro dos Santos Junior <devaluizio@gmail.com>
2024-06-29 15:28:37 -0300

    Tipo de variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        short num1 = 1;
+        int num2 = num1;
+        short num3 = (short) num2;
+
+        final double PI =  3.1415;
+
+        float numero = 2.500F;
+        System.out.println("Hello, World!"+numero);
     }
 }
