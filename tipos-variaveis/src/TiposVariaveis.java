4f30a22a364fa7dcc9419e8d8e9e42917a179c73
Aluizio Monteiro dos Santos Junior <devaluizio@gmail.com>
2024-06-30 07:00:09 -0300

    Incremento, decremento e negação

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -5,13 +5,26 @@ public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         
 
-        String concatenation = "?";
-        System.out.println(concatenation);
-        concatenation = 1+1+1+"1";
-        System.out.println(concatenation);
-        concatenation = 1+"1"+1+1;
-        System.out.println(concatenation);
-        concatenation = "1"+(1+1+1);
-        System.out.println(concatenation);
+        boolean value = true;
+        System.out.println(value);
+        System.out.println(!value);
+        System.out.println(!!value);
+        System.out.println(!!!value);
+        value = !value;
+        System.out.println("Reverse: "+value);
+        // int number = 5;
+        // System.out.println(number++);
+        // System.out.println(++number);
+        // System.out.println(number);
+        // System.out.println(- --number);
+        
+        // int number = 5;
+        // number = -number;
+        // System.out.println(number);
+        // System.out.println(--number);
+        // System.out.println(number--);
+        // System.out.println(++number);
+        // System.out.println(number++);
+        // System.out.println(number * -1);
     }
 }
