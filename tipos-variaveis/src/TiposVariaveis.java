f65567f824224f4cf49b358bde608a08d3087f43
Aluizio Monteiro dos Santos Junior <devaluizio@gmail.com>
2024-06-30 11:27:19 -0300

    Operadores Lógicos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -3,48 +3,20 @@
 
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        String name = "Monteiro";
-        String name1 = "Monteiro";
-        String name2 = new String("Monteiro");
-
-        /*
-         * The "==" operators, when used on class objects, 
-         * compare values ​​and references. Therefore, "==" 
-         * is only recommended for primitive type comparisons.
-        */
-        System.out.println(name == name1);
-        System.out.println(name == name2);
-        /*
-         * Comparisons with the equals() method look at 
-         * the contents of the variables and not their 
-         * memory addresses.
-        */
-        System.out.println(name.equals(name1));
-        System.out.println(name.equals(name2));
         
-        // int a = 3, b = 4;
+        boolean STATUS1 = true;
+        boolean STATUS2 = false;
 
-        // System.out.println(a == b);
-        // System.out.println(a != b);
-        // System.out.println(a > b);
-        // System.out.println(a < b);
+        if (STATUS1 && STATUS2){
+            System.out.println("Both expressions are true.");
+        } else if (STATUS1 || STATUS2){
+            System.out.println("Only one of the two expressions is true.");
+        }
 
-        // int a = 3, b = 4;
-        // String result = "";
+        if (STATUS1 && !STATUS2){
+            System.out.println("They were forced to be equals.");
+        }
 
-        // switch (Integer.compare(a, b)) {
-        //     case 0:
-        //         result = "A is equal to B";
-        //         break;
-        //     case 1:
-        //         result = "A is greater than B";
-        //         break;
-        //     case -1:
-        //         result = "A is smaller than B";
-        //         break;
-        //     default:
-        //         break;
-        // }
-        // System.out.println(result);
+        System.out.println(STATUS1 && (3 > 9));
     }
 }
