3f32fd4685a64433f02d01a1319cac550674e239
Aluizio Monteiro dos Santos Junior <devaluizio@gmail.com>
2024-06-30 09:46:43 -0300

    Operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -3,29 +3,48 @@
 
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        
-        int a, b;
-        String result = "";
-        a = 5;
-        b = 6;
+        String name = "Monteiro";
+        String name1 = "Monteiro";
+        String name2 = new String("Monteiro");
 
-        // if (a == b){
-        //     result = "true";
-        // }else{
-        //     result = "false";
-        // }
-
-        result = a == b ? "true" : "false";
-        System.out.println("Result: "+result);
-
-        /*The ternary operator can be used with 
-        * expressions that are not Boolean, 
-        * as long as the result type is compatible 
-        * with the expected return value types
-        *
+        /*
+         * The "==" operators, when used on class objects, 
+         * compare values ​​and references. Therefore, "==" 
+         * is only recommended for primitive type comparisons.
         */
+        System.out.println(name == name1);
+        System.out.println(name == name2);
+        /*
+         * Comparisons with the equals() method look at 
+         * the contents of the variables and not their 
+         * memory addresses.
+        */
+        System.out.println(name.equals(name1));
+        System.out.println(name.equals(name2));
+        
+        // int a = 3, b = 4;
 
-        int biggest = (a > b)? a : b;
-        System.out.println(biggest+" is the biggest number");
-        }
+        // System.out.println(a == b);
+        // System.out.println(a != b);
+        // System.out.println(a > b);
+        // System.out.println(a < b);
+
+        // int a = 3, b = 4;
+        // String result = "";
+
+        // switch (Integer.compare(a, b)) {
+        //     case 0:
+        //         result = "A is equal to B";
+        //         break;
+        //     case 1:
+        //         result = "A is greater than B";
+        //         break;
+        //     case -1:
+        //         result = "A is smaller than B";
+        //         break;
+        //     default:
+        //         break;
+        // }
+        // System.out.println(result);
+    }
 }
